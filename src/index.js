8beb8681f6d5f083f6e0479c972c5904685352e0
dlepperts <danielle.leppertsimenauer@gmail.com>
2021-02-06 18:11:37 -0600

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addToList()
 });
+
+function addToList() {
+
+  const form = document.getElementById("create-task-form")
+  const submit = form.getElementsByTagName("input")[1];
+
+  submit.addEventListener('click', function(){
+    const ul = document.getElementById("tasks");
+    const li = document.createElement('li');
+    const input = form.getElementsByTagName("input")[0]
+    
+    li.innerHTML = `<p>${input.value}</p><button>Delete Task</button>`;
+    ul.appendChild(li);
+    input.value = ""
+
+    deleteTask()
+  })
+}
+
+function deleteTask() {
+
+  const removeTask = document.getElementsByTagName("button");
+
+  removeTask[0].addEventListener('click', function(event) {
+    event.preventDefault();
+    event.currentTarget.parentNode.remove();
+  })
+}
